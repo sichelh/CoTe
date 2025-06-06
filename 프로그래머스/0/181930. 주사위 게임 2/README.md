f285c475738886f16f9839a5d3b67a07cce348ac
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-06-07 01:17:43 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.24 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=csharp) 
 
 ### 성능 요약
 
-메모리: 4.12 MB, 시간: 0.01 ms
+메모리: 31.6 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 07일 00:57:39
+2025년 06월 07일 01:17:41
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a!=b && b!=c && a!=c)
+        {
+            answer = a+b+c;
+        }
+        else if (a!=b || a!=c || b!=c)
+        {
+            answer = (a+b+c) * (a*a+b*b+c*c);  
+        }
+        else{
+            answer = (a+b+c) * (a*a+b*b+c*c) * (a*a*a+b*b*b+c*c*c);
+        }
+        return answer;
+    }
+}
